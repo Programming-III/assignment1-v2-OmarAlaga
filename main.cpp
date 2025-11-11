7f74ae6e8437656e6c78498a7a94c5f8dfaef3b7
OmarAlaga <omaralaga4444@gmail.com>
2025-11-11 09:47:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,39 +8,61 @@
 
 using namespace std;
 
-// ==================== Person Class Implementation =========================
+public :
+
+Person(string name , int id)
+{
+    this->name = name;
+    this->id = id;
+};
+
+Course( string courseCode , string courseName , int maxStudents , Student* students ,int currentStudents )
+{
+    this->courseCode = courseCode;
+    this->courseName = courseName;
+    this->maxStudents = maxStudents;
+    this->students = students;
+    this->currentStudents = currentStudents;
+};
+
+student(int yearLevel , string major )
+{
+    this->yearLevel = yearLevel;
+    this->major = major;
+};
+
+Instructor(string department , int experienceYears; )
+{
+    this->department = department;
+    this->experienceYears = experienceYears;
+}
 
 
+string Person :: display()
+{
+    cout << "Name :" << " " << name << endl;
+    cout << "ID :" << " " << id << endl;
+};
 
+string Instructor :: display()
+{
+    cout << "Department : " << " " << department << endl;
+    cout << "Experiance Years :" << " " << experienceYears << endl;
+};
 
+Course :: void addStudent(const Student& s)
+{
+  return currentStudents + s;  
+};
 
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
+string Course :: displayCourseInfo()
+{
+    cout << "Course Code :" << " " << courseCode << endl;
+    cout << "Course Name : " << " " << courseName << endl;
+    cout << "Max Students : " << " " << maxStudents << endl;
+    cout << "Student : " << " " << students << endl;
+    cout << "Current Students :" << " " << currentStudents << endl;
+};
 int main() {
    
     
